a5f524576c8db58528c9dfa004e4f78ce4c920ac
Lucas Guimaraes <lucas@sazsaude.com.br>
2023-09-09 15:39:30 -0300

    feat: add controller for directions

--- backend/src/maps/directions/directions.controller.ts
+++ backend/src/maps/directions/directions.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { DirectionsService } from './directions.service';
 
 @Controller('directions')
-export class DirectionsController {}
+export class DirectionsController {
+  constructor(private directionService: DirectionsService) {}
+
+  @Get()
+  getDirections(
+    @Query('originId') originId: string,
+    @Query('destinationId') destinationId: string,
+  ) {
+    return this.directionService.getDirections(originId, destinationId);
+  }
+}
