f0fb1be0e39b14816d07608ff7daa4bb5cd67207
Lucas H. B. Guimaraes <lucas@sazsaude.com.br>
2023-11-16 17:26:50 -0300

    feat: new entries for routes moduel

--- backend/src/routes/routes.module.ts
+++ backend/src/routes/routes.module.ts
@@ -3,10 +3,18 @@ import { RoutesService } from './routes.service';
 import { RoutesController } from './routes.controller';
 import { MapsModule } from 'src/maps/maps.module';
 import { RoutesDriverService } from './routes-driver/routes-driver.service';
+import { RoutesGateway } from './routes/routes.gateway';
+import { BullModule } from '@nestjs/bull';
+import { WEBSOCKET_NEW_POINTS } from 'src/utils/constants';
 
 @Module({
-  imports: [MapsModule],
+  imports: [
+    MapsModule,
+    BullModule.registerQueue({
+      name: WEBSOCKET_NEW_POINTS,
+    }),
+  ],
   controllers: [RoutesController],
-  providers: [RoutesService, RoutesDriverService],
+  providers: [RoutesService, RoutesDriverService, RoutesGateway],
 })
 export class RoutesModule {}
